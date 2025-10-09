370ae6dae8941271752686d1be11dcfb315887aa
mannsk1302 <138506116+mannsk1302@users.noreply.github.com>
2025-10-09 17:41:56 +0530

    feat: enhance mongoose connection, add development tools, and dynamic routes
    - Update mongoose connection to utilize dynamic MongoDB URI from `config` package
    - Add `debug` for enhanced logging in development mode
    - Introduce conditional owner creation route in development environment
    - Add `config` and `debug` as project dependencies
    - Update `.gitignore` to include development configuration files

--- Project-Folder/routes/ownersRouter.js
+++ Project-Folder/routes/ownersRouter.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const ownerModel = require('../models/owners-model');
 
 router.get('/', (req, res) => {
-    res.send('Owners');
+    res.send('Owners 1');
 });
+
+if(process.env.NODE_ENV === 'development'){
+    router.post('/create', async (req, res) => {
+        let owners = await ownerModel.find()
+        if(owners.length > 0){
+            res.send('Owner already exists')
+        }
+        let {fullName, email, password} = req.body;
+        let createdOwner = await ownerModel.create({
+            fullName,
+            email,
+            password
+        });
+        res.status(201).send(createdOwner);
+    });
+}
 
 module.exports = router;
